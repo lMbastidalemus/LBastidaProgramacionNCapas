a14aef1eb006c12654b2df2a2be78ade82bcb0db
lMbastidalemus <lbastidalemus@gmail.com>
2023-11-08 17:22:19 -0600

    Listas

--- PL_MVC/App_Start/RouteConfig.cs
+++ PL_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PL_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Usuario", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Usuario", action = "GetAll", id = UrlParameter.Optional }
             );
         }
     }
